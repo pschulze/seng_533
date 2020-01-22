c3394ca22ad24cfc4df38e6564392a4ba870251c
Peter Schulze <peterkschulze@gmail.com>
2020-01-22 13:45:35 -0700

    add routes

--- src/seng_533.cr
+++ src/seng_533.cr
@@ -1,4 +1,6 @@
+require "flexo"
 require "kemal"
+require "uuid"
 
 post "/compare" do |env|
   file_1 = env.params.files["image1"].tempfile
@@ -13,7 +15,16 @@ post "/compare" do |env|
     IO.copy(file_2, f)
   end
 
-  "Upload ok"
+  result = Flexo.compare(file_path_1, file_path_2)
+  result_file_uuid = UUID.random.to_s
+  result.save(::File.join [Kemal.config.public_folder, "results/", result_file_uuid])
+
+  env.response.content_type = "application/json"
+  {score: result.score, comparison_image_path: "https://seng533.pschulze.dev/download/#{result_file_uuid}"}.to_json
+end
+
+get "/result/:guid" do |env|
+  send_file(env, ::File.join [Kemal.config.public_folder, "results/", env.params.url["guid"]])
 end
 
 Kemal.run
